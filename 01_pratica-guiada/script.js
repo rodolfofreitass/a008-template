798969828ea3e3d454fbbdbc4f75d88d47a9af17
rodolfofreitass <rodolfofreitas38@hotmail.com>
2022-07-20 21:31:35 -0300

    exercicios feitos da aula a008

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,62 @@
 //CODIGO AQUI
+console.log("teste")
+
+let saldo = 15 + 22.40
+console.log("saldo inicial", saldo)
+
+saldo = saldo - 12.34
+console.log("saldo subtraído", saldo)
+
+saldo = saldo / 3
+console.log("saldo para cada irmão", saldo)
+
+saldo = saldo * 3.42
+console.log("investimento", saldo)
+
+saldo = saldo % 3
+console.log("resto do chiclete", saldo)
+
+const multESoma = 2*2+2
+console.log(multESoma)
+const somaEMult = 2*(2+2)
+console.log(somaEMult)
+
+const testeSomas = 2*(4+5+(12/4+(100-50+(3-(10)))))
+console.log(testeSomas)
+
+let comparador = 6 === 6
+console.log(comparador)
+
+let comparador2 = 5!== 5
+console.log(comparador2)
+
+let comparador3 = 5!== "5"
+console.log(comparador3)
+
+let comparador4 = "5" === "cinco"
+// deu false pq msm as duas sendo iguais quanto a string, eles tem valores dif
+console.log(comparador4)
+
+let comparador5 = typeof 5 === typeof 20
+console.log(comparador5)
+
+let comparador6 = typeof "5" === typeof "cinco"
+console.log(comparador6)
+
+const num1 = 2
+const num2 = 8
+let resultado
+
+resultado = num1 == num2
+console.log(resultado)
+
+resultado = num1 <= num2
+console.log(resultado)
+
+resultado = num1 > num2
+console.log(resultado)
+
+resultado = num1 < num2
+console.log(resultado)
+
+
